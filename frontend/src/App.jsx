50185897490a2827799572d66bf69179d936399a
Anika Tasnim <anikatasnim1066@gmail.com>
2026-08-23 22:26:47 +0600

    set up frontend and backend MVC project structure for ride request feature

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import RideRequest from "./components/RideRequest";
+
+function App() {
+  return (
+    <main>
+      <h1>TransitHubJU</h1>
+
+      <RideRequest />
+    </main>
+  );
+}
+
+export default App;
